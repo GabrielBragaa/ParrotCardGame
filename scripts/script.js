7472602dfd9b8b6b5190391e74297b9d6a5dc75f
GabrielBragaa <gbragamoura@hotmail.com>
2023-04-05 23:53:37 -0300

    implementada a função de virar e desvirar as cartas

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,30 @@
-let qtd;
-
-
 function qtdCarta() {
     const deck = document.querySelector('.deck');
     let contador = 0;
-    qtd = prompt('Com quantas cartas quer jogar?');
+    let qtd = prompt('Com quantas cartas quer jogar?');
     while (qtd < 4 || qtd > 14 || qtd % 2 !== 0) {
         qtd = prompt('Com quantas cartas quer jogar?');
     } 
 
     while (contador < qtd) {
-        deck.innerHTML += '<div class="carta"><div class="face frente"><img src="./img/back.png" alt=""></div><div class="face verso"><img src="" alt=""></div></div>';
+        deck.innerHTML += '<div class="carta" onclick="virarCarta(this)"><div class="face frente"><img src="./img/back.png" alt=""></div><div class="face verso"><img src="" alt=""></div></div>';
         contador++;
     }
 }
 
+
+function virarCarta(carta) {
+    const frente = carta.querySelector('.frente');
+    frente.classList.add('vira-frente');
+    const verso = carta.querySelector('.verso');
+    verso.classList.add('vira-verso');
+    function voltaCarta() {
+        const front = carta.querySelector('.frente');
+        front.classList.remove('vira-frente');
+        const back = carta.querySelector('.verso')
+        back.classList.remove('vira-verso')
+    }
+    setTimeout(voltaCarta, 1000);
+}
 
 qtdCarta();
