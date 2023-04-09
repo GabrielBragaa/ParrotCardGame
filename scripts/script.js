c99871afdecfaddc4b4ddba138782b8a24117314
GabrielBragaa <gbragamoura@hotmail.com>
2023-04-09 18:40:16 -0300

    melhorias de código SonarQube

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,10 @@
-const imagens = ['<img src="./img/bobrossparrot.gif" alt="" data-test="face-up-image"></img>',
-'<img src="./img/explodyparrot.gif" alt="" data-test="face-up-image"></img>',
-'<img src="./img/fiestaparrot.gif" alt="" data-test="face-up-image"></img>',
-'<img src="./img/metalparrot.gif" alt="" data-test="face-up-image"></img>',
-'<img src="./img/revertitparrot.gif" alt="" data-test="face-up-image"></img>',
-'<img src="./img/tripletsparrot.gif" alt="" data-test="face-up-image"></img>',
-'<img src="./img/unicornparrot.gif" alt="" data-test="face-up-image"></img>'];
+const imagens = ['<img src="./img/bobrossparrot.gif" alt="" data-test="face-up-image"></img>', '<img src="./img/explodyparrot.gif" alt="" data-test="face-up-image"></img>', '<img src="./img/fiestaparrot.gif" alt="" data-test="face-up-image"></img>', '<img src="./img/metalparrot.gif" alt="" data-test="face-up-image"></img>', '<img src="./img/revertitparrot.gif" alt="" data-test="face-up-image"></img>', '<img src="./img/tripletsparrot.gif" alt="" data-test="face-up-image"></img>', '<img src="./img/unicornparrot.gif" alt="" data-test="face-up-image"></img>'];
 let cartasViradas = 0, imgSelecionadas = [], primeiraCarta, segundaCarta, pontos = 0, tempo = 1;
+const intervalo = '';
 
 function qtdCarta() {
     const deck = document.querySelector('.deck');
-    var qtd = prompt('Com quantas cartas quer jogar?');
+    qtd = prompt('Com quantas cartas quer jogar?');
     while (qtd < 4 || qtd > 14 || qtd % 2 !== 0) {
         qtd = prompt('Com quantas cartas quer jogar?');
     }
@@ -25,7 +20,7 @@ function qtdCarta() {
     for (let contador = 0; contador < qtd; contador++) {
         deck.innerHTML += `<div class="carta" onclick="virarCarta(this)" data-test="card"><div class="face frente"><img src="./img/back.png" alt="" data-test="face-down-image"></div><div class="face verso">${imgSelecionadas[contador]}</div></div>`;
     }
-    var intervalo = setInterval(cronometro, 1000);
+    intervalo = setInterval(cronometro, 1000);
 }
 
 function virarCarta(carta) {
@@ -69,10 +64,9 @@ function resetCarta() {
 }
 
 function cronometro() {
-    var elemento = document.querySelector('.cronometro');
+    const elemento = document.querySelector('.cronometro');
     elemento.innerText = '';
-    tempo++;
-    elemento.innerText = tempo;
+    elemento.innerText = tempo++;
 }
 
 function resetJogo() {
@@ -80,6 +74,7 @@ function resetJogo() {
     imgSelecionadas = [];
     pontos = 0;
     tempo = 1;
+
     const deck = document.querySelector('.deck');
     deck.innerHTML = '';
 }
